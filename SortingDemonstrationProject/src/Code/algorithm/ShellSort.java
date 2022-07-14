efa1fe568ab2580bd6a6e55cce2a0455f7014076
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-07-14 13:11:32 +0700

    Update ShellSort.java

--- SortingDemonstrationProject/src/Code/algorithm/ShellSort.java
+++ SortingDemonstrationProject/src/Code/algorithm/ShellSort.java
@@ -2,65 +2,39 @@ package algorithm;
 
 import historylog.HistoryLog;
 
-public class ShellSort extends Algorithm{
-	//the idea is to sort on a partially sorted array
+public class ShellSort extends Algorithm {
+	@Override
 	public String sort() {
-		int n = arr.length;
-		this.numberOfIteration = 1;
-		int correctIndexFrom = 0;
+		numberOfIteration = 0;
 		
-		//inner and outer refers to 2 integers in each iteration; inner < outer
-		int inner, outer, temp;
-		int interval = 1;
+		int[] gaps = {701, 301, 132, 57, 23, 10, 4, 1};  // Ciura gap sequence
 		
-		//we can choose different interval but this is a very common way
-		while(interval <= n / 3) {
-			interval = interval * 3 + 1;
-		}
-		while (interval > 0) {
-			for (outer = interval; outer < n; outer++) {
-				temp = arr[outer];
-				inner = outer;
+		for (int gap: gaps) {
+			
+			for (int i = gap; i < arr.length; i++) {
+				int temp = arr[i];
 				
-				
-				if (!(inner > interval - 1 && arr[inner - interval] >= temp)){
-					//history log here
-					
-					//deep copy array at current state
-					int[] currentArr = new int[arr.length];
-					for(int k = 0; k < arr.length; k++){
-						currentArr[k] = arr[k];
+				int j = i;
+				for (; j - gap >= 0; j -= gap) {
+					// log writing
+					historyLogs.add(new HistoryLog(
+							arr.clone(),
+							new int[] {arr[j], arr[j-gap]},
+							new int[] {j, j-gap},
+							++numberOfIteration,
+							0));
+						System.out.println(gap);
+					if (arr[j - gap] > temp) {
+						arr[j] = arr[j - gap];
+						arr[j - gap] = temp;  // redundant in algorithm but needed for demonstration
+					} else {
+						break;
 					}
-					
-					historyLogs.add(new HistoryLog(currentArr, new int[] {arr[inner - interval], arr[outer]}, new int[] {inner - interval, outer}, numberOfIteration++, correctIndexFrom));
-//					System.out.println("For: " + Arrays.toString(currentArr));
 				}
-		
-				while(inner > interval - 1 && arr[inner - interval] >= temp) {
-					//history log here
-					
-					//deep copy array at current state
-					int[] currentArr1 = new int[arr.length];
-					for(int k = 0; k < arr.length; k++){
-						currentArr1[k] = arr[k];
-					}
-				
-					
-					
-					arr[inner] = arr[inner - interval];
-					historyLogs.add(new HistoryLog(currentArr1, new int[] {arr[inner - interval], temp}, new int[] {inner - interval, outer}, numberOfIteration++, correctIndexFrom));
-//					System.out.println("While: " + Arrays.toString(currentArr1));
-						
-					
-					inner = inner - interval;
-				}
-				if (interval == 1) {
-					correctIndexFrom++;
-				}
-				arr[inner] = temp;
+				// arr[j] = temp;  // this can be removed because of the redundancy above, lol
 			}
-			interval = (interval - 1) / 3;
 		}
+		
 		return "";
 	}
 	
