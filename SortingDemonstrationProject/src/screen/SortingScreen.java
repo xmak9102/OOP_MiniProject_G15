ff61199f90b1719631db0a0d83614ed9dca62643
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-07-15 11:53:23 +0700

    return the old Sylfaen font

--- SortingDemonstrationProject/src/screen/SortingScreen.java
+++ SortingDemonstrationProject/src/screen/SortingScreen.java
@@ -71,7 +71,7 @@ public class SortingScreen extends GeneralScreen {
 		
 		//ColorPane helps us to change the colors of the text easily
 		sortingColorPane = new ColorPane();
-		sortingColorPane.setFont(new Font("MONOSPACED", Font.PLAIN, 23));
+		sortingColorPane.setFont(new Font("Sylfaen", Font.PLAIN, 23));  // "MONOSPACED"
 		sortingColorPane.setBackground(Color.WHITE);
 		sortingColorPane.setText("Creating a new array to start sorting!");
 		sortingColorPane.setEditable(false);
